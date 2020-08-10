fce67840d4b9b46a4d77a60bef86afb7bad6d15c
Adjeko <adjeko88@gmail.com>
2020-08-10 20:56:34 +0200

    create personal task in cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,26 +11,107 @@ admin.initializeApp();
 // });
 
 
-
+// ##############################################
+// ############### LOGIN / SIGNUP ###############
+// ##############################################
 exports.createProfile = functions
     .region('europe-west3')
     .auth.user()
     .onCreate((user) => {
 
-        const profiles = admin.firestore().collection('profiles');
+        const profiles = admin.firestore().collection('player');
 
+        //create new profile with two subcollections
         return profiles.doc(user.uid).set({
             id: user.uid,
             tasks: [],
             groups: [],
-        });
+        }).then(() => admin.firestore().doc(`player/${user.uid}/tasks/testTask`).set({
+            name: "testTask"
+        })).then(() => admin.firestore().doc(`player/${user.uid}/groups/testGroup`).set({
+            name: "testGroup"
+        }));
     });
 
 exports.deleteProfile = functions
     .region('europe-west3')
     .auth.user()
     .onDelete((user) => {
-        const profiles = admin.firestore().collection('profiles');
+        const profiles = admin.firestore().collection('player');
 
         return profiles.doc(user.uid).delete();
     });
+
+
+
+// ##############################################
+// ############# PERSONAL TASKS #############
+// ##############################################
+exports.addPersonalTask = functions
+    .region('europe-west3')
+    .firestore.document('player/{userId}/tasks/{taskId}')
+    .onCreate((snap, context) => {
+        const userId = context.params.userId;
+        const taskId = context.params.taskId;
+
+        const newValue = snap.data();
+
+        if (!newValue.userId) {
+            newValue.userId = userId;
+        }
+        if (!newValue.taskId) {
+            newValue.taskId = taskId;
+        }
+
+
+        return Promise.all([
+            //fulfill the request by creating new task in player/tasks.
+            snap.ref.set(newValue, { merge: true }),
+            //add task to the index array in the player profile itself.
+            admin.firestore().doc(`player/${userId}`).update({
+                tasks: admin.firestore.FieldValue.arrayUnion(newValue)
+            })
+        ]);
+
+    });
+// exports.updatePersonalTask = functions.region('europe-west3')
+// exports.deletePersonalTask = functions.region('europe-west3')
+
+// ##############################################
+// ################# GROUPS #################
+// ##############################################
+// exports.addGroup = functions
+//     .region('europe-west3')
+//     .firestore.document('groups/{groupId}')
+//     .onCreate((snap, context) => {
+//         const userId = context.params.userId;
+//         const groupId = context.params.groupId;
+
+//         const newValue = snap.data();
+
+//         if (!newValue.taskId) {
+//             newValue.taskId = groupId;
+//         }
+
+//         return Promise.all([
+//             snap.ref.set(newValue, { merge: true }),
+//             admin.firestore().doc(`player/${userId}`).update({
+//                 groups: admin.firestore.FieldValue.arrayUnion(newValue)
+//             })
+//         ]);
+//     });
+// exports.updateGroup = functions.region('europe-west3')
+// exports.deleteGroup = functions.region('europe-west3')
+
+// ##############################################
+// ############# TASKS IN GROUPS ############
+// ##############################################
+// exports.addGroupTask = functions.region('europe-west3')
+// exports.updateGroupTask = functions.region('europe-west3')
+// exports.deleteGroupTask = functions.region('europe-west3')
+
+// ##############################################
+// ############## MEMBERS IN GROUPS #############
+// ##############################################
+// exports.joinGroup = functions.region('europe-west3')
+// exports.leaveGroup = functions.region('europe-west3')
